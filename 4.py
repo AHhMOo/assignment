fb106eee504defb345bed6551e6f43e1cb520e27
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2024-05-09 15:37:32 -0400

    4.py

---
+++ 4.py
@@ -0,0 +1,14 @@
+# question 3 task 1
+bread = 7
+peanut_butter = 5
+jelly = 8
+the_total_cost = (bread + peanut_butter + jelly)
+print (the_total_cost)
+
+
+# question 3 task 2
+initial_amount = 10000
+interest_rate = 7/100 
+total_amount = initial_amount + (initial_amount * interest_rate)
+print (total_amount)
+
