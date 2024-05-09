fd964e8c8960024ff876ee67129fab48244675be
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2024-05-09 15:29:42 -0400

    1.py

---
+++ 1.py
@@ -0,0 +1,18 @@
+# question 1 task 1 
+weather = "sunny" 
+
+if weather == "sunny":
+  print("Wear sunglasses!")
+else:
+  print("Take an umbrella!")
+
+
+#question 1 task 2
+mood = input("how you feeling today?")
+
+if mood == "happy":
+  print("That's great to hear!")
+elif mood =="sad":
+  print("I hope your day gets better!")
+
+
